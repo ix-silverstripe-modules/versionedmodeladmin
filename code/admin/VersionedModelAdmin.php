da7c8c1fef4c1029e0b938426bdf0215374e316d
Stewart Wilson <stewartwilson@outlook.com>
2015-04-24 11:54:04 +1000

    code

---
+++ code/admin/VersionedModelAdmin.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * this is for page or versioned page Model Admin
+ * 
+ * data objects like News, Store, Event and Product should be managed in ModelAdmin which extended from VersionedModelAdmin
+ * 
+ * 
+ * For example.
+ * 
+	class NewsAdmin extends VersionedModelAdmin {
+		
+		private static $title       = 'News';
+		private static $menu_title  = 'News';
+		private static $url_segment = 'news';
+	
+		private static $managed_models  = array('News');
+		private static $model_importers = array();
+		
+	}
+ *
+ *	
+ */
+abstract class VersionedModelAdmin extends ModelAdmin {
+
+	public function init(){
+		
+		parent::init();
+		
+		Versioned::reading_stage("Stage");
+	}
+	
+	
+	public function getEditForm($id = null, $fields = null){
+		$form = parent::getEditForm($id, $fields);
+		
+		$GridField = $form->Fields()->dataFieldByName($this->sanitiseClassName($this->modelClass));
+		
+		$GridField
+			->getConfig()
+			->removeComponentsByType('GridFieldDeleteAction')
+			->addComponent(new GridFieldVersionedModelDeleteAction())
+		;
+		
+		return $form;
+	}
+	
+	
+	
+	public function getSearchContext() {
+		
+		$context = parent::getSearchContext();
+		
+		$modelClass = $this->modelClass;
+			
+		if($modelClass::has_extension('Versioned')) {
+			$context
+				->getFields()
+				->push(
+					DropdownField::create('q[Status]', 'Status', array('Published' => "Published", 'Unpublished' => "Unpublished"))
+						->setHasEmptyDefault(true)
+				);
+		}
+		
+		return $context;
+	}
+	
+	public function getList() {
+		$list = parent::getList();
+		
+		$params = $this->request->requestVar('q'); // use this to access search parameters
+		
+		$modelClass = $this->modelClass;
+	
+		if($modelClass::has_extension('Versioned')) {
+	
+			if(isset($params['Status']) && $params['Status']){
+				$tableName = $modelClass;
+				
+				$ids = DB::query("SELECT \"ID\" FROM \"{$tableName}_Live\"")->keyedColumn();
+					
+				if($params['Status'] == "Published"){
+					$list = $list->filter('ID', $ids);
+				}else{
+					$list = $list->exclude('ID', $ids);
+				}
+			}
+		}
+	
+		return $list;
+	}
+	
+	
+}
+
+
+
+?>
---
+++ code/formfields/gridfield/GridFieldVersionedModelDeleteAction.php
@@ -0,0 +1,55 @@
+<?php
+class GridFieldVersionedModelDeleteAction extends GridFieldDeleteAction {
+
+	/**
+	 * Handle the actions and apply any changes to the GridField
+	 *
+	 * @param GridField $gridField
+	 * @param string $actionName
+	 * @param mixed $arguments
+	 * @param array $data - form data
+	 * @return void
+	 */
+	public function handleAction(GridField $gridField, $actionName, $arguments, $data) {
+		if($actionName == 'deleterecord' || $actionName == 'unlinkrelation') {
+			$item = $gridField->getList()->byID($arguments['RecordID']);
+			if(!$item) {
+				return;
+			}
+
+			if($actionName == 'deleterecord') {
+				$member = Member::currentUserID();
+
+				if($item instanceof SiteTree){
+					//if it's a page. call doUnpublish() before delete it.
+					if( ! $item->canDeleteFromLive($member)){
+						throw new ValidationException('No delete permissions for live site',0);
+					}
+						
+					$item->doUnpublish();
+						
+				}elseif ($item->hasExtension('Versioned')){
+					//if data object has versioned extension. delete the live record first.
+
+					$item->deleteFromStage('Live');
+				}
+
+				//ok. do the normal delete().
+				if(!$item->canDelete()) {
+					throw new ValidationException(
+							_t('GridFieldAction_Delete.DeletePermissionsFailure',"No delete permissions"),0);
+				}
+
+				$item->delete();
+			} else {
+				if(!$item->canEdit()) {
+					throw new ValidationException(
+							_t('GridFieldAction_Delete.EditPermissionsFailure',"No permission to unlink record"),0);
+				}
+
+				$gridField->getList()->remove($item);
+			}
+		}
+	}
+
+}
